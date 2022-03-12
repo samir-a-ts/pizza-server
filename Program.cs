ed0f6b33d0c30b260b317dace9b6fccca0adf2a1
lutrak.developer <lutrak.developer@gmail.com>
2022-03-12 18:17:38 +0200

    Still trying to fix docker.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost
+    .UseKestrel()
+    .UseContentRoot(Directory.GetCurrentDirectory())
+    .UseIISIntegration()
+    .UseUrls("http://0.0.0.0:" + builder.Configuration["PORT"]);
+
 Injection.Init(builder.Services, builder.Configuration);
 
 var app = builder.Build();
